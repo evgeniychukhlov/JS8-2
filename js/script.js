0016fce557c725d4298f408294d24406cfb91812
Evgeniy Chukhlov <evgeniy.chukhlov@gmail.com>
2023-08-21 15:50:33 +0300

    на перевірку

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Задача 1
+// const userPassword = prompt("Введіть пароль:");
+// const isValidPassword = validatePassword(userPassword);
+
+// console.log(isValidPassword); 
+
+// function validatePassword(password) {
+//     if (password.length < 8) {
+//         return false;
+//     }
+
+//     const firstChar = password.charAt(0);
+//     if (!isNaN(parseInt(firstChar))) {
+//         return true;
+//     }
+
+// }
+// Задача 2
+
+// let userAmount = prompt("Вкажіть сумму в ГРН (лише число)");
+// let rate = 36.91; 
+  
+// let amountUSD = userAmount/rate;
+
+// console.log("Ваша сума",userAmount,"UAH");
+// console.log("Конвертована сума",Math.round(amountUSD),"USD" );
+
+// Задача 3
+
+// const petrolAmount = prompt("Вкажіть бажану кількість літрів бензину");
+// const petrolPrise = prompt ("Вкажіть вартість бензину за 1л");
+// const totalPrice = petrolAmount*petrolPrise; 
+// console.log ("Вартість складатиме", Math.round(totalPrice),"UAH");
